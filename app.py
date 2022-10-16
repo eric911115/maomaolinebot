acf5ccd5c3a16e708d90768726aae8774e50851e
eric911115 <aeric911115@gmail.com>
2022-10-17 01:55:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def movie():
         if index == 20:
             return content       
         title = data.text
-        content += '{}+\n'.format(title).strip()
+        content += '{}\n'.format(title).lstrip()
     return content
 
 @handler.add(MessageEvent, message=TextMessage)
