80c2ba18fd1628812de28470a70beeb12ffff303
eric911115 <aeric911115@gmail.com>
2022-11-07 15:12:25 +0800

    Update bug.py

--- bug.py
+++ bug.py
@@ -1,20 +1,14 @@
 from importlib.resources import contents
 import requests 
 from bs4 import BeautifulSoup
-from urllib.request import urlretrieve 
-import requests.packages.urllib3
-requests.packages.urllib3.disable_warnings()
+import urllib.request as req
+import json
 
+url="https://www.fight30.com/products/mango-jump-towel"
 
-    
-target_url = 'https://www.fight30.com/products/mango-jump-towel'
-rs = requests.session()
-res = rs.get(target_url, verify=False)
-res.encoding = 'utf-8'
-soup = BeautifulSoup(res.text, 'html.parser')   
-content = ""
-for index, data in enumerate(soup.select('div.out-of-stock txt-sold-out')):
-    if index == 20:      
-        title = data.text
-        content += '{}\n'.format(title).lstrip()
-    print(soup.title)
+with req.urlopen(url)as response:
+    data=response.read().decode("utf-8")
+ 
+root=BeautifulSoup(data,"html.parser")
+titles=root.find("div",class_="out-of-stock txt-sold-out")
+print(titles.text)
